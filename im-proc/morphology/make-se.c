daf28e7433082ff56d36da229ef5c48c12d141d9
zychen <johnny96boy@gmail.com>
2020-04-03 18:08:04 +0200

    morphologie implement se and make-se

--- im-proc/morphology/make-se.c
+++ im-proc/morphology/make-se.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 #include <morphology.h>
-
+#include <se.h>
 void
 usage(char* s)
 {
@@ -15,7 +15,11 @@ int
 main(int argc, char* argv[])
 {
   if(argc != PARAM+1) usage(argv[0]);
-  (void)argc;
-  (void)argv;
+  int shape = atoi(argv[1]);
+  int hs = atoi(argv[2]);
+  char * filename = argv[3];
+  pnm imd = se(shape,hs);
+  pnm_save(imd,PnmRawPpm,filename);
+  pnm_free(imd);
   return EXIT_SUCCESS;
 }
--- im-proc/morphology/se.c
+++ im-proc/morphology/se.c
@@ -8,7 +8,92 @@ enum {SQUARE, DIAMOND, DISK, LINE_V, DIAG_R, LINE_H, DIAG_L, CROSS, PLUS};
 
 pnm
 se(int s, int hs){  
-  (void)s;
-  (void)hs;
-  return NULL;
+  int size = 2 * hs + 1;
+  pnm imd = pnm_new(size,size,PnmRawPpm);
+
+  switch (s)
+  {
+  case SQUARE:
+    for (int i = 0; i < size; i++)
+      for (int j = 0; j < size; j++)
+        for (int k = 0; k < 3; k++)
+          pnm_set_component(imd,i,j,k,255);
+    break;
+  case DIAMOND:
+    for (int i = 0; i < size; i++)
+    {
+      int interval = (i<=hs) ? i : size-i;
+      int start = hs-interval;
+      int end = hs+ interval;
+      for(int j = start; j<end;j++)
+        for(int k =0; k<3; k++)
+          pnm_set_component(imd,i,j,k,255);
+    }
+    break;
+  case DISK:
+    for (int i = 0; i < size; i++)
+      for (int j = 0; j < size; j++){
+        int d = sqrt((i-hs)*(i-hs)+(j-hs)*(j-hs));
+        if(d<hs)
+          for (int k = 0; k < 3; k++)
+            pnm_set_component(imd,i,j,k,255);
+      }
+    break;
+  case LINE_V:
+    for (int i = 0; i < size; i++)
+      for (int k = 0; k < 3; k++)
+        pnm_set_component(imd,i,hs,k,255);
+    break;
+  case DIAG_R:
+    for (int i = 0; i < size; i++)
+      for (int j = 0; j < size; j++){
+        if (i==size-j)
+          for (int k = 0; k < 3; k++)
+            pnm_set_component(imd, i, j, k, 255);
+      }
+    break;
+  case LINE_H:
+    for (int j = 0; j < size; j++)
+      for (int k = 0; k < 3; k++)
+        pnm_set_component(imd, hs, j, k, 255);
+    break;
+  case DIAG_L:
+    for (int i = 0; i < size; i++)
+      for (int j = 0; j < size; j++)
+      {
+        if (i ==  j)
+          for (int k = 0; k < 3; k++)
+            pnm_set_component(imd, i, j, k, 255);
+      }
+    break;
+  case CROSS:
+    for (int i = 0; i < size; i++)
+      for (int j = 0; j < size; j++)
+      {
+        if (i == size - j)
+          for (int k = 0; k < 3; k++)
+            pnm_set_component(imd, i, j, k, 255);
+      }
+    for (int i = 0; i < size; i++)
+      for (int j = 0; j < size; j++)
+      {
+        if (i == j)
+          for (int k = 0; k < 3; k++)
+            pnm_set_component(imd, i, j, k, 255);
+      }
+    break;
+  case PLUS:
+    for (int i = 0; i < size; i++)
+      for (int k = 0; k < 3; k++)
+        pnm_set_component(imd, i, hs, k, 255);
+    for (int j = 0; j < size; j++)
+      for (int k = 0; k < 3; k++)
+        pnm_set_component(imd, hs, j, k, 255);
+    break;
+  
+  default:
+    break;
+  }
+
+  return imd;
 }
